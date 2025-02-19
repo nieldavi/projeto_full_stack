d0ade3349d77784cddff3654e81cb0263fecfd1a
Francisco Daniel <franciscodanielbct@gmail.com>
2025-02-19 20:37:51 -0300

    aula02

--- cadastro/admin.py
+++ cadastro/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from cadastro.models import Curso,Estudante
 # Register your models here.
+
+class Estudantes(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'cpf', 'email','data_nascimento', 'celular', 'endereco')
+    list_display_links = ('id', 'nome',)
+    list_per_page = 20 
+    search_fields = ('nome','cpf',)
+
+admin.site.register(Estudante,Estudantes)
